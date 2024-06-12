32cd7a348af328fbea7cc81c526d194e3aaac8a0
raspberryflapper <95499257+raspberryflapper@users.noreply.github.com>
2024-06-12 16:28:25 +0000

    normalized with 72. 1_75_1_1_0.03 produces a fit. The partial deriv calculations are in a single for loop. Cleaned up the code.

--- gradient_descent_2024.py
+++ gradient_descent_2024.py
@@ -23,15 +23,18 @@ def main():
 
     #store x values (time in minutes) into array t
     t = np.array([data[0] for data in data_1a])
+    t = t/72
     #store y values into array y
     y = np.array([data[1] for data in data_1a])
 
+    """
     #normalize the x's
     norm_t = []
     for i  in range(len(t)):
         new_t = (t[i]/720)
         norm_t.append(new_t)
         #print(norm_t[i])
+    """
 
     #print("now the y's")
 
@@ -50,7 +53,7 @@ def main():
     a = float(input("Initial value for a: "))
     b = float(input("Initial value for b: "))
     c = float(input("Initial value for c: "))
-    d = float(input("Initial value for d:"))
+    d = float(input("Initial value for d: "))
     step_size = float(input("Step size: "))
     #store user initial guess for running the program again
     initial_a = a
@@ -72,50 +75,49 @@ def main():
         partial_a = 0
         partial_b = 0
         partial_c = 0
+        partial_d = 0
         
-        #summation for each partial derivatives
-        for i in range(len(norm_t)):
-            partial_a += a*np.exp(-2*b*np.exp(-c*norm_t[i]))-norm_y[i]*np.exp(-b*np.exp(-c*norm_t[i]))
-        for i in range(len(norm_t)):
-            partial_b += -a*a*np.exp(-c*norm_t[i]-2*b*np.exp(-c*norm_t[i]))+norm_y[i]*a*np.exp(-c*norm_t[i]-b*np.exp(-c*norm_t[i]))
-        for i in range(len(norm_t)):
-            partial_c += a*a*b*norm_t[i]*np.exp(-c*norm_t[i]-2*b*np.exp(-c*norm_t[i]))-norm_y[i]*a*b*norm_t[i]*np.exp(-c*norm_t[i]-b*np.exp(-c*norm_t[i]))
         for i in range(len(t)):
-            partial_d += 2*(a*np.exp(-b*np.exp(-c*norm_t[i])) + d - norm_y[i])
+            #this is the e^(-b*e^(-c*t)) term
+            exp_term_no_coeff = np.exp(-b*np.exp(-c*t[i]))
+            #3 terms in side the sigma
+            inside_terms = a*np.exp(-b*np.exp(-c*t[i]))+d-norm_y[i]
+            #this is the e^((-c*t)-(b*e^(-c*t))) term
+            exp_term_minus_ct = np.exp((-c*t[i])-b*np.exp(-c*t[i]))
+            #summations for each partials
+            partial_a += 2*(inside_terms)*exp_term_no_coeff
+            partial_b += 2*(inside_terms)*-a*exp_term_minus_ct
+            partial_c += 2*(inside_terms)*exp_term_minus_ct*a*b*t[i]
+            partial_d += 2*(inside_terms)
+
         #gradient descent algorithm:
         #update the parameters a,b, and c
-        a = prev_a - step_size*partial_a
-        b = prev_b - step_size*partial_b
-        c = prev_c - step_size*partial_c
-        d = prev_d - step_size*partial_d
+        a = prev_a  - (step_size * partial_a)
+        b = prev_b  - (step_size * partial_b)
+        c = prev_c  - (step_size * partial_c)
+        d = prev_d  - (step_size * partial_d)
 
-        if abs(partial_a)<0.00001 and abs(partial_b)<0.00001 and abs(partial_c)<0.00001:
+        if abs(partial_a)<0.0001 and abs(partial_b)<0.0001 and abs(partial_c)<0.0001 and abs(partial_d)<0.0001:
             print (f"After {iteration}th interations: ")
             print (f"Parameter a is: {a} and partial A is {partial_a}")
             print (f"Parameter b is: {b} and partial B is {partial_b}")
             print (f"Parameter c is: {c} and partial C is {partial_c}")
+            print (f"Parameter d is: {d} and partial C is {partial_d}")
             #Use the parameters found here for the function f(norm_t). Get and store the output values for the graph
-            x = np.linspace(0,1,100)
+            x = np.linspace(0,10,100)
             f_t = []
             for i in range(len(x)):
                 function_t = a * np.exp(-b * np.exp(-c * x[i]))+d
                 f_t.append(function_t)
-            graphData(norm_t,norm_y,x,f_t)#calls the graphing function and graphs
+            graphData(t,norm_y,x,f_t)
             break
         
         elif iteration >500000:
             print("Too many iterations")
-            print(initial_a,initial_b,initial_c)
-            print("initial a, b, and c above.")
+            print(initial_a,initial_b,initial_c,initial_d)
+            print("initial a, b, c, and d above.")
             print(step_size)
             print("step size above.")
-            x = np.linspace(0,1,100)
-            f_t = []
-            for i in range(len(x)):
-                function_t = a * np.exp(-b * np.exp(c * x[i]))+d
-                f_t.append(function_t)
-            graphData(norm_t,norm_y,x,f_t)
-            break
 
         prev_a = a
         prev_b = b
